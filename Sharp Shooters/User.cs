a1c860d5bc4e3de8545512dfde6cdaa92261cb4f
Eddie Halling <exel_eddie@hotmail.com>
2023-11-22 13:29:23 +0100

    Added 2 base classes (Bank, User) and one sub-class (Admin, inherits from User).

---
+++ Sharp Shooters/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharp_Shooters
+{
+    internal class User
+    {
+
+    }
+}
