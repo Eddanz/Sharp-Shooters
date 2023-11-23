7dcc5a5d75c920c8a0197790db3540cb3cdd2f90
Eddie Halling <exel_eddie@hotmail.com>
2023-11-23 14:25:10 +0100

    Changed name on MainMenu to WelcomeMenu, added new mainMenu with switch cases to acess the different options. Added a Run method to start the program in Main. Removed InternalTransfer andf ExternalTransfer, added Transfer instead where you can do both, and added logic to it.

--- Sharp Shooters/Bank.cs
+++ Sharp Shooters/Bank.cs
@@ -4,20 +4,103 @@ namespace Sharp_Shooters
 {
     internal class Bank
     {
-        public static void MainMenu()
+        static void WelcomeMenu()
         {
-            bool isLoggedIn = false;
-            
-            
-                Console.WriteLine("Welcome to SharpShooter Bank");
-                Console.WriteLine("----------------------------");
-                Console.WriteLine("                 _ _.-'`-._ _\r\n                ;.'________'.;\r\n     _________n.[____________].n_________\r\n    |\"\"_\"\"_\"\"_\"\"||==||==||==||\"\"_\"\"_\"\"_\"\"]\r\n    |\"\"\"\"\"\"\"\"\"\"\"||..||..||..||\"\"\"\"\"\"\"\"\"\"\"|\r\n    |LI LI LI LI||LI||LI||LI||LI LI LI LI|\r\n    |.. .. .. ..||..||..||..||.. .. .. ..|\r\n    |LI LI LI LI||LI||LI||LI||LI LI LI LI|\r\n ,,;;,;;;,;;;,;;;,;;;,;;;,;;;,;;,;;;,;;;,;;,,\r\n;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;");
-                Console.WriteLine("\nPress Enter to Log in!");
-                Console.ReadKey();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Welcome to SharpShooter Bank");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("                 _ _.-'`-._ _\r\n                ;.'________'.;\r\n     _________n.[____________].n_________\r\n    |\"\"_\"\"_\"\"_\"\"||==||==||==||\"\"_\"\"_\"\"_\"\"]\r\n    |\"\"\"\"\"\"\"\"\"\"\"||..||..||..||\"\"\"\"\"\"\"\"\"\"\"|\r\n    |LI LI LI LI||LI||LI||LI||LI LI LI LI|\r\n    |.. .. .. ..||..||..||..||.. .. .. ..|\r\n    |LI LI LI LI||LI||LI||LI||LI LI LI LI|\r\n ,,;;,;;;,;;;,;;;,;;;,;;;,;;;,;;,;;;,;;;,;;,,\r\n;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;");
+            Console.WriteLine("\nPress Enter to Log in!");
+            Console.ReadKey();
+        }
+
+        static void MainMenu(User loggedInUser, List<User> users)
+        {
+            while (true)
+            {
                 Console.Clear();
-            
+                Console.Write($"\n===== You are logged in as: {loggedInUser.UserName.ToUpper()} =====" +
+                    $"\n\nMake a choise below" +
+                    $"\n[1] Manage accounts" +
+                    $"\n[2] Set up a loan" +
+                    $"\n[3] Transfer" +
+                    $"\n[4] Log out" +
+                    $"\n\nCHOISE: ");
 
+                string userChoise = Console.ReadLine();
+                switch (userChoise)
+                {
+                    case "1":
+                        Console.Clear();
+                        User.AccountOverview(loggedInUser);
+                        Console.WriteLine("\n[1] Open a new account" +
+                            "\n[2] Go back to main menu");
+                        string userChoise1 = Console.ReadLine();
+                        switch (userChoise1)
+                        {
+                            case "1":
+                                Console.Clear();
+                                Console.WriteLine("[1] Open a regular account" +
+                                    "\n[2] Open a savings account");
+                                string accountType = Console.ReadLine();
+                                switch (accountType)
+                                {
+                                    case "1":
+                                        User.OpenNewAccount(loggedInUser);
+                                        break;
+                                    case "2":
+                                        User.OpenSavingsAccount(loggedInUser);
+                                        break;
+                                    default:
+                                        Console.Clear();
+                                        Console.WriteLine("\nChoose between 1-2!");
+                                        Thread.Sleep(2000);
+                                        break;
+                                }
+                                break;
+                            case "2":
+                                break;
+                            default:
+                                Console.Clear();
+                                Console.WriteLine("\nChoose between 1-2!");
+                                Thread.Sleep(2000);
+                                break;
+                        }
+                        break;
+                    case "2":
+                        //User.LoanMoney();
+                        break;
+                    case "3":
+                        Console.Clear();
+                        User.Transfer(loggedInUser, users);
+                        break;
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine("You are now logged out...");
+                        Thread.Sleep(3000);
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("\nChoose between 1-4!");
+                        Thread.Sleep(2000);
+                        break;
+                }
+            }
+        }
 
+        public static void Run()
+        {
+            List<User> users = Admin.InitializeUser();
+            while (true)
+            {
+                WelcomeMenu();
+                User loggedInUser = User.LogIn(users);
+                if (loggedInUser != null)
+                {
+                    MainMenu(loggedInUser, users);
+                }
+            }
         }
     }
 }
--- Sharp Shooters/Program.cs
+++ Sharp Shooters/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace Sharp_Shooters
+    
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Bank.MainMenu();
+            Bank.Run();
         }
     }
 }
